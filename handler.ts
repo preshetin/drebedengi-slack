f8ed4dcf8392063bc77c29a94763aa82eda796e8
Petr Reshetin <preshetin@gmail.com>
2022-05-06 17:01:20 +0300

    working balance and crate income

--- handler.ts
+++ handler.ts
@@ -1,4 +1,7 @@
 import * as _ from 'lodash'
+import {ApiClient} from './ts-ddng-client/src/index'
+import { GetBalanceParams } from './ts-ddng-client/src/messages/getBalance'
+import { CreateExpenseParams } from './ts-ddng-client/src/messages/setRecordList'
 
 // modern module syntax
 export async function hello(event, context, callback) {
@@ -6,13 +9,30 @@ export async function hello(event, context, callback) {
   // dependencies work as expected
   console.log(_.VERSION)
 
+  const client = new ApiClient('demo_api', 'demo@example.com', 'demo')
+
+//   const params: GetBalanceParams = { };
+//   const balances = await client.getBalance(params);
+//   console.log('balances', balances);
+
+  const createExpenseParams: CreateExpenseParams = {
+      placeId: 40034,
+      comment: 'cool drug',
+      sum: 322222,
+      currencyId: 17,
+      categoryId: 40012
+  }
+  const createExpenseResult = await client.createExpense(createExpenseParams)
+  console.log('createExenseResult', createExpenseResult)
+
+
   // async/await also works out of the box
   await new Promise((resolve, reject) => setTimeout(resolve, 500))
 
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: 'Go 11 Serverless v1.0! Your function executed successfully!',
       input: event,
     }),
   };
