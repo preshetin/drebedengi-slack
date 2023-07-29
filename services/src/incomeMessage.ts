7df081ccdc08c52b23b37c85f7515a34960c14c6
Petr Reshetin <preshetin@gmail.com>
2023-07-29 13:28:53 +0500

    feat: formatted sum in income message

--- services/src/incomeMessage.ts
+++ services/src/incomeMessage.ts
@@ -11,7 +11,8 @@ export function incomeMessage(
   balanceCurrency: string,
   user: string
 ): MessageBlocks {
-  const sum = values.sum.sum.value;
+  const sum = +values.sum.sum.value;
+  const sumFormatted = sum.toLocaleString();
 
   const currency = values.currencyId.currencyId.selected_option.text.text;
   const source = values.sourceId.sourceId.selected_option.text.text;
@@ -48,13 +49,13 @@ export function incomeMessage(
   // +1000 RUB на _Сбербанк_. (источник _Консультации Светы_), ввел(а) preshetin ```Комментарий:\nПеревод по СБП. ФИО отправителя: Алексей Сергеевич Г.\nДата: 2022-06-15```
 
   return {
-    text: `+${sum} ${currency} доход :moneybag: в место хранения ${place}. Баланс ${balanceSum} ${balanceCurrency}. Источник ${source}, ввел(а) <@${user}>`,
+    text: `+${sumFormatted} ${currency} доход :moneybag: в место хранения ${place}. Баланс ${balanceSum.toLocaleString()} ${balanceCurrency}. Источник ${source}, ввел(а) <@${user}>`,
     blocks: [
       {
         type: "section",
         text: {
           type: "mrkdwn",
-          text: `+${sum} ${currency} доход :moneybag: в место хранения ${place}. Баланс ${balanceSum} ${balanceCurrency}. Источник ${source}, ввел(а) <@${user}> ${detailsText}`,
+          text: `+${sumFormatted} ${currency} доход :moneybag: в место хранения ${place}. Баланс ${balanceSum.toLocaleString()} ${balanceCurrency}. Источник ${source}, ввел(а) <@${user}> ${detailsText}`,
         },
       },
     ],
